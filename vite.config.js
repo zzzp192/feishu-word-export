6aff128e32c8a55314c4493180f904b95ae82c25
zzzp192 <133836409+zzzp192@users.noreply.github.com>
2026-01-09 17:32:00 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
+  base: '/feishu-word-export/',
   server: {
     host: '0.0.0.0',
   }
